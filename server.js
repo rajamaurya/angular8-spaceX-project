1a145e14e92671393e68aafe59eac9297e4ebbe6
RAJA KRISHNA MAURYA <krishna.raja78@gmail.com>
2021-02-06 00:36:32 +0530

    cors update

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ app.use(function (req, res, next) {
 
     // Website you wish to allow to connect
     res.setHeader('Access-Control-Allow-Origin', 'https://spacex-musk.herokuapp.com/');
-    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:4200');
+    //res.setHeader('Access-Control-Allow-Origin', 'http://localhost:4200');
 
     // Request methods you wish to allow
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
